81bbdba2ce1080e3a510f7e39163c0157e8f883d
boithanh <boithanh01694@gmail.com>
2025-06-15 22:50:52 +0700

    Fix resposive in medium screen

--- src/js/banner.js
+++ src/js/banner.js
@@ -3,8 +3,8 @@ export const banner = () => {
     return `<div class="video-container">
  <img src="/fallbackBanner.jpg" alt="IT Banner Preview" />
   <div class="overlay flex justify-center justify-items-center flex-col mx-auto">
-    <div class="max-sm:w-full sm:w-7/12 mx-auto text-center">
-      <h1 class="mb-4 text-white max-sm:text-3xl sm:text-5xl">Reliable <span>IT Help Desk Support</span>
+    <div class="max-sm:w-full md:w-full xl:w-8/12 mx-auto text-center">
+      <h1 class="mb-4 text-white max-sm:text-3xl sm:text-4xl lg:text-5xl">Reliable <span>IT Help Desk Support</span>
         <br>Providing efficient solutions,
         <br>technical assistance, <b>&</b> <span> seamless user support</span>
         <br>to keep systems<span> running smoothly</span>.
@@ -15,12 +15,12 @@ export const banner = () => {
   }
   return `<div class="video-container">
   <video autoplay muted playsinline loop>
-    <source src="/74933-554074594_small.mp4" type="video/mp4">
+    <source src="/61037-497754241.mp4" type="video/mp4">
     Your browser does not support the video tag.
   </video>
   <div class="overlay flex justify-center justify-items-center flex-col mx-auto">
-    <div class="max-sm:w-full sm:w-7/12 mx-auto text-center">
-      <h1 class="mb-4 text-white max-sm:text-3xl sm:text-5xl">Reliable <span>IT Help Desk Support</span>
+    <div class="max-sm:w-full md:w-full xl:w-8/12 mx-auto text-center">
+      <h1 class="mb-4 text-white max-sm:text-3xl sm:text-4xl lg:text-5xl">Reliable <span>IT Help Desk Support</span>
         <br>Providing efficient solutions,
         <br>technical assistance, <b>&</b> <span> seamless user support</span>
         <br>to keep systems<span> running smoothly</span>.
